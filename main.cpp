bcf6a25cba2ebf34a577fca6f85c3a86d4a8bd7e
Martin Kubovčík <markub3327@gmail.com>
2020-09-21 20:19:11 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-// MazeSolver_v3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <chrono>
 #include <thread>
@@ -18,17 +15,18 @@ using namespace AI;
 using namespace std;
 
 
-#define VELKOST_POPULACIE						 (1000) 	// pocet jedincov
+#define VELKOST_POPULACIE						  (128) 	// pocet jedincov
 #define POCET_GENOV								    (2)
 #define POCET_KROKOV							  (100)
-#define POCET_GENERACII						   (100000)
+#define POCET_GENERACII						   (1000000)
 
 int main(int argc, char** argv)
 {
 	Player _robot;
 	Environment* _env = new Environment();
 	int done;
-	//int x1, y1;
+	float best_duration;
+	int best_is_end;
 	FILE *f_fitness;
 
 	f_fitness = fopen("fitness.txt", "w");
@@ -66,7 +64,7 @@ int main(int argc, char** argv)
 				{
 					_robot.setReward(-50.0);
 					_robot.Move(oldX, oldY);
-					done = 1;
+					done = -1;
 				}
 				else
 					// Zisti odmenu za akciu a[t]
@@ -75,30 +73,10 @@ int main(int argc, char** argv)
 				// Zaznamenaj odmenu do celkovej sily jedinca
 				_ai->setFitnessOfIndividual(individual_index, _robot.getReward());
 
-				if ((generation % 100 == 0) && (individual_index == _ai->getBest()))
-				{
-					// Nakresli herny svet
-					_env->NakresliHernySvet(_robot.getX(), _robot.getY());
-
-					std::cout << "Generation: " << generation << std::endl;
-					std::cout << "Individual: " << individual_index << std::endl;
-					std::cout << "*	 robot.X = " << _robot.getX() << std::endl;
-					std::cout << "*	 robot.Y = " << _robot.getY() << std::endl;
-					std::cout << "*	 action = " << _ai->getGeneOfIndividual(individual_index, _env->getState(_robot.getX(), _robot.getY())) << std::endl;
-					std::cout << "*	 step = " << step << std::endl;
-					std::cout << "*	 done = " << done << std::endl;
-					std::cout << "*	 fitness = " << _ai->getFitnessOfIndividual(individual_index) << std::endl;
-					std::cout << "*	 mutation_rate = " << _ai->getMutationRate() << std::endl;
-				}
-
 				// ak konec hry
 				if (done != 0) break;
 			}
 		}
-
-		auto score = _ai->getFitnessOfIndividual(_ai->getBest());
-		if (generation % 100 == 0 && score > -50.0)
-			fprintf(f_fitness, "%f\n", score);
 
 		// Najdi najlepsieho jedinca (jeho vlastnosti sa nebudu menit)
 		_ai->updateBest();
@@ -110,39 +88,69 @@ int main(int argc, char** argv)
 		_ai->Mutation();
 	}
 
-	// Pause
-	//system("pause");
-
 	//  Testing agent 
-	/*for (int time = 0; time < 5; time++)
+	for (int time, step = 0; time < 1000; time++)
 	{
+		auto t_start = std::chrono::high_resolution_clock::now(); 
+
+		// clear fitness
+		_ai->clearFitness();
+		best_is_end = 0;
+
 		// pociatocny stav
 		auto idx = rand() % 3;
 		_robot.Move(startPositionsX[idx], startPositionsY[idx]);
 
 		// Nakresli herny svet
-		_env->NakresliHernySvet(_robot.getX(), _robot.getY());
+		//_env->NakresliHernySvet(_robot.getX(), _robot.getY());
 
-		do {
+		for (step = 0; step < POCET_KROKOV; step++)
+		{
+			auto oldX = _robot.getX();
+			auto oldY = _robot.getY();
+
 			// Vykonaj akciu
 			_robot.Move(_ai->getGeneOfIndividual(_ai->getBest(), _env->getState(_robot.getX(), _robot.getY())));
 
-			// Zisti odmenu za akciu a[t]
-			_env->getReward(_robot.getX(), _robot.getY(), &done);
+			if (_robot.getX() < 0 || _robot.getX() > (_env->getWidth()-1) || _robot.getY() < 0 || _robot.getY() > (_env->getHeight()-1))
+			{
+				_robot.setReward(-50.0);
+				_robot.Move(oldX, oldY);
+				done = -1;
+			}
+			else
+				// Zisti odmenu za akciu a[t]
+				_robot.setReward(_env->getReward(_robot.getX(), _robot.getY(), &done));
 
-			// casova medzera pre usera
-			this_thread::sleep_for(std::chrono::milliseconds(250));
-			_env->NakresliHernySvet(_robot.getX(), _robot.getY());
+			// Zaznamenaj odmenu do celkovej sily jedinca
+			_ai->setFitnessOfIndividual(_ai->getBest(), _robot.getReward());
 
-			std::cout << "robot.X = " << _robot.getX() << std::endl;
-			std::cout << "robot.Y = " << _robot.getY() << std::endl;
-			std::cout << "action = " << _ai->getGeneOfIndividual(_ai->getBest(), _env->getState(_robot.getX(), _robot.getY())) << std::endl;
+			// nasiel vychod
+			if (done == 1)
+				best_is_end = 100;
 
-		} while (done != -1);
+			//std::cout << "Generation: " << time << std::endl;
+			//std::cout << "Individual: " << _ai->getBest() << std::endl;
+			//std::cout << "*	 robot.X = " << _robot.getX() << std::endl;
+			//std::cout << "*	 robot.Y = " << _robot.getY() << std::endl;
+			//std::cout << "*	 action = " << _ai->getGeneOfIndividual(_ai->getBest(), _env->getState(_robot.getX(), _robot.getY())) << std::endl;
+			//std::cout << "*	 step = " << step << std::endl;
+			//std::cout << "*	 done = " << done << std::endl;
+			//std::cout << "*	 fitness = " << _ai->getFitnessOfIndividual(_ai->getBest()) << std::endl;
+			//std::cout << "*	 mutation_rate = " << _ai->getMutationRate() << std::endl;
+			//std::cout << "*	 time = " << best_duration << " us" << std::endl;
+
+			// ak konec hry
+			if (done != 0) break;
+		}
+
+		auto t_stop = std::chrono::high_resolution_clock::now(); 
+		// floating-point duration: no duration_cast needed
+    	std::chrono::duration<float, std::milli> fp_ms = t_stop - t_start;
+		best_duration = fp_ms.count() * 1000;
+
+		fprintf(f_fitness, "%d;%f;%d;%f;%d\n", time, _ai->getFitnessOfIndividual(_ai->getBest()), step, best_duration, best_is_end);
 	}
-
-	// Pause
-	system("pause");*/
 
 	fclose(f_fitness);
 
